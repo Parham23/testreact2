9b0956fb84c89925818766b90aa4cbc9b3b8015e
Parham23 <parhamborji@gmail.com>
2024-10-07 01:49:26 -0700

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>E-commerce originated in a standard for the exchange of business documents, such as orders or invoices, between suppliers and their business customers. Those origins date to the 1948–49 Berlin blockade and airlift with a system of ordering goods primarily via telex. Various industries elaborated upon that system in the ensuing decades before the first general standard was published in 1975. The resulting computer-to-computer electronic data interchange (EDI) standard is flexible enough to handle most simple electronic business transactions.</p>
+            <p>With the wide adoption of the Internet and the introduction of the World Wide Web in 1991 and of the first browser for accessing it in 1993, most e-commerce shifted to the Internet. More recently, with the global spread of smartphones and the accessibility of fast broadband connections to the Internet, much e-commerce moved to mobile devices, which also included tablets, laptops, and wearable products such as watches.</p>
+        </div>
+    </div>
+  )
+}
